c057794d856208607aab25811ca22e402696f711
Masoma123628 <masoma.egov@gmail.com>
2025-07-10 11:24:09 +0600

    Update GoogleSearchTest.java

--- jenkinsCiCD/src/test/java/tests/GoogleSearchTest.java
+++ jenkinsCiCD/src/test/java/tests/GoogleSearchTest.java
@@ -41,33 +41,33 @@ public class GoogleSearchTest {
         }
     }
     
-    // @Test
-    // public void testGoogleSearch() {
-    //     page.navigate("https://www.google.com");
-    //     page.locator("textarea[name='q']").fill("Playwright Java");
-    //     page.keyboard().press("Enter");
-    //     page.waitForTimeout(3000);  // wait for results
-    //     assert page.title().toLowerCase().contains("playwright java");
-    // }
+    @Test
+    public void testGoogleSearch() {
+        page.navigate("https://www.google.com");
+        page.locator("textarea[name='q']").fill("Playwright Java");
+        page.keyboard().press("Enter");
+        page.waitForTimeout(3000);  // wait for results
+        assert page.title().toLowerCase().contains("playwright java");
+    }
 
-    // @Test
-    // public void testLoginSauceDemo() {
-    //     // Navigate to Sauce Demo login page
-    //     page.navigate("https://www.saucedemo.com/v1/");
+    @Test
+    public void testLoginSauceDemo() {
+        // Navigate to Sauce Demo login page
+        page.navigate("https://www.saucedemo.com/v1/");
         
-    //     // Fill in login credentials
-    //     page.locator("input#user-name").fill("standard_user");
-    //     page.locator("input#password").fill("secret_sauce");
+        // Fill in login credentials
+        page.locator("input#user-name").fill("standard_user");
+        page.locator("input#password").fill("secret_sauce");
         
-    //     // Click the login button
-    //     page.locator("input[type='submit']").click();
+        // Click the login button
+        page.locator("input[type='submit']").click();
         
-    //     // Wait for the login to process
-    //     page.waitForSelector(".inventory_list"); // Wait for the inventory page to load
+        // Wait for the login to process
+        page.waitForSelector(".inventory_list"); // Wait for the inventory page to load
         
-    //     // Assert that the user has successfully logged in by verifying a visible element on the dashboard
-    //     assert page.locator(".inventory_list").isVisible();
-    // }
+        // Assert that the user has successfully logged in by verifying a visible element on the dashboard
+        assert page.locator(".inventory_list").isVisible();
+    }
 
     @AfterMethod
     public void teardown() {
